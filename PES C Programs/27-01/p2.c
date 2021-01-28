1219e9b00bc43faa4a6ef0036f3cd07435804ab5
ParikshithV <Parikv99@gmail.com>
2021-01-28 11:33:44 +0530

    28-01

--- PES C Programs/27-01/p2.c
+++ PES C Programs/27-01/p2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-  int a[10][12], r, c;
+  int a[10][12], r, c, ua;
   printf("Assuming base address as 2400 \n");
   printf("Enter a[r][c] \n");
   printf("a[r]:");
@@ -12,14 +12,22 @@ int main() {
   //bp = &a[0][0];    //Change to *bp at line 14 & 11 if bp is defined here
   bp = 2400;    //Assuming base address as 2400, Change to bp at line 14 & 11
   int ba = bp;
-  printf("\nBase address: %d\n",ba);
+  //printf("\nBase address: %d\n",ba);
   int offsetr = r*12+c;
   int offsetc = c*10+r;
   int addressr = offsetr*4+ba;
   int addressc = offsetc*4+ba;
-  printf("Offset in row major: %d\n",offsetr);
-  printf("Offset in column major: %d\n",offsetc);
-  printf("Address in row major: %d\n",addressr);
-  printf("address in column major: %d\n",addressc);
+
+  printf("Enter predicted address of element: ");
+  scanf("%d",&ua);
+  if(addressr == ua){
+    printf("\nMatrix is row-major");
+  }
+  else if(addressc == ua){
+    printf("\nMatrix is column-major");
+  }
+  else{
+    printf("\npredicted address is invalid");
+  }
   return 0;
 }
