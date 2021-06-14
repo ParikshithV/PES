61d7ef4d267167f2e143fe45428b64c0687c339b
Parikshith <parikv99@gmail.com>
2021-06-14 13:48:30 +0530

    L2 P1

---
+++ 2nd Sem MCA/java/lab2/p1.java
@@ -0,0 +1,31 @@
+class Statbloc {
+    static {
+      System.out.println("Static block called ");
+    }
+
+    public static String greet="Instance variable";
+
+    public static void main(String args[]) {
+      {
+        System.out.println("Instance block");
+      }
+
+      System.out.println(Statbloc.greet);
+      Constr1 c1 = new Constr1();
+      Constr2 c2 = new Constr2("hello");
+    }
+}
+
+class Constr1 {
+  public Constr1() {
+    System.out.println("Constructor called without paramater");
+  }
+}
+
+class Constr2 {
+  public static String greet="Constructor parameter";
+  public Constr2(String param) {
+    String greeting = param;
+    System.out.println("Constructor called with paramater: "+greeting);
+  }
+}
