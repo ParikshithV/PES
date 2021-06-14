acf2ac279d4316fe82704fc36c0d21d3c8ba925d
Parikshith <parikv99@gmail.com>
2021-06-14 15:01:04 +0530

    Update fibo.java

--- 2nd Sem MCA/java/lab2/fibo.java
+++ 2nd Sem MCA/java/lab2/fibo.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Fibo{
   public static void main(String args[])
   {
-     int num1,num2=0,num3,i,ulim,llim;
+     int num1=1,num2=0,num3,i,ulim,llim;
 
      Scanner input= new Scanner(System.in);
 
@@ -15,11 +15,10 @@ class Fibo{
 
      if (ulim>llim) {
        System.out.println("Fibonacci series:");
-       num1 = llim;
-       for(i=llim;i<ulim;++i)
+       for(i=1;i<ulim;++i)
        {
           num3=num1+num2;
-          if(num3<ulim)
+          if(num3<ulim && num3>llim)
             System.out.print(num3+", ");
           num1=num2;
           num2=num3;
