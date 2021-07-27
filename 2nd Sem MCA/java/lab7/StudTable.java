55b505aa0d0e1438b3acf724a71909fa838a3825
Parikshith <parikv99@gmail.com>
2021-07-27 18:06:30 +0530

    Update StudTable.java

--- 2nd Sem MCA/java/lab7/StudTable.java
+++ 2nd Sem MCA/java/lab7/StudTable.java
@@ -10,12 +10,14 @@ class StudTable {
 		Statement st=con.createStatement();
 
     String dropTable = "DROP TABLE student";
-    st.executeQuery(dropTable);
+    //st.executeQuery(dropTable);
 
     String TableCreate ="CREATE TABLE student (srn varchar(10), name varchar(10), course varchar(10), cgpa varchar(10))";
-    st.executeQuery(TableCreate);
+    //st.executeQuery(TableCreate);
 
     String query="select * from student";
+    String query1="select * from student where cgpa > 8";
+    String query2="select * from student where cgpa < 5";
 
     Scanner sc = new Scanner(System.in);
 
@@ -43,14 +45,24 @@ class StudTable {
       c= sc.nextLine().charAt(0);
     }
 
-		ResultSet result=st.executeQuery(query);
-    System.out.println("\nSRN\tName\tCourse\tCGPA");
 
-    int i = 0;
-
+    System.out.println("Original table:\nSRN\tName\tCourse\tCGPA");
+    ResultSet result=st.executeQuery(query);
 		while(result.next()){
 			System.out.println(result.getInt(1)+"\t"+result.getString(2)+"\t"+result.getString(3)+"\t"+result.getString(4));
 		}
+
+    System.out.println("\nCGPA > 8:\nSRN\tName\tCourse\tCGPA");
+    ResultSet result1=st.executeQuery(query1);
+		while(result1.next()){
+			System.out.println(result1.getInt(1)+"\t"+result1.getString(2)+"\t"+result1.getString(3)+"\t"+result1.getString(4));
+		}
+
+    System.out.println("\nCGPA < 5:\nSRN\tName\tCourse\tCGPA");
+    ResultSet result2=st.executeQuery(query2);
+    while(result2.next()){
+      System.out.println(result2.getInt(1)+"\t"+result2.getString(2)+"\t"+result2.getString(3)+"\t"+result2.getString(4));
+    }
 
 		con.close();
 	}
