94fab2a654c1edcd756054e6a5ee36011e75c8c3
ParikshithV <56472103+ParikshithV@users.noreply.github.com>
2021-11-10 04:00:37 +0530

    Delete 3rd Sem MCA directory

--- 3rd Sem MCA/reactclass/src/components/EventBind.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class EventBind extends React.Components(){
-    return(){
-        <div></div>
-    }
-}
-
-export default EventBind;
--- 3rd Sem MCA/reactclass/src/reverse.js
+++
@@ -1,5 +0,0 @@
-// import React from 'react';
-
-export default function reverse(){
-    this.setState(this.state.items.reverse())
-}
